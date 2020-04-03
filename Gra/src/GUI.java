0bc90696251c9f4f6fa6d271b80cfa1bee93ce55
Jan Lozinski <jasioloza@gmail.com>
2020-04-03 20:52:58 +0200

    Poprawione okno główne GUI

--- Gra/src/GUI.java
+++ Gra/src/GUI.java
@@ -1,11 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,7 +21,7 @@ import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
 
-public class GUI extends JFrame implements ActionListener {
+public class GUI extends JFrame {
 
 	JPanel lewy, prawy, gorny;
 	JLabel labelszybkosc, labelpaliwo;
@@ -31,78 +34,76 @@ public class GUI extends JFrame implements ActionListener {
 	
 	public GUI() throws HeadlessException {
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-			this.setSize(600,480);
-				this.setTitle("Kosmiczne Podró¿e");
-		lewy = new JPanel();
-			lewy.setLayout(new BoxLayout(lewy, 3));
+			
+			this.setTitle("Kosmiczne Podró¿e");
+			lewy = new JPanel();
+			lewy.setLayout(new BoxLayout(lewy, BoxLayout.PAGE_AXIS));
+			lewy.setPreferredSize(new Dimension(200,600));
+			lewy.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+			
 			startbutton = new JButton("START");
-				startbutton.setPreferredSize(new Dimension(40, 100));
+		    startbutton.setMaximumSize(new Dimension(150,100));
+		    
 			//add ActionListener ktory bedzie wyrzucal okienko na srodek
-				lewy.add(startbutton);
-		labelszybkosc = new JLabel("Szybkoœæ:");
-				lewy.add(labelszybkosc);
+			startbutton.setAlignmentX(Component.CENTER_ALIGNMENT);
+			lewy.add(startbutton);
+			
+			lewy.add(Box.createRigidArea(new Dimension(0,150)));
+			
+			labelszybkosc = new JLabel("Szybkoœæ:");
+			labelszybkosc.setAlignmentX(Component.CENTER_ALIGNMENT);
+			lewy.add(labelszybkosc);
+			
 			szybkosc = new JTextField();
-				lewy.add(szybkosc);
-		labelpaliwo = new JLabel("Poziom paliwa:");
-				lewy.add(labelpaliwo);
+			szybkosc.setMaximumSize(new Dimension(100,40));
+			szybkosc.setAlignmentX(Component.CENTER_ALIGNMENT);
+			lewy.add(szybkosc);
+			lewy.add(Box.createRigidArea(new Dimension(0,20)));
+			
+			labelpaliwo = new JLabel("Poziom paliwa:");
+			labelpaliwo.setAlignmentX(Component.CENTER_ALIGNMENT);
+			lewy.add(labelpaliwo);
+			
 			slider = new JSlider(JSlider.VERTICAL, 0, 100, 20);
-				slider.setPaintTicks(true);
-				slider.setPaintLabels(true);
-				slider.setMinorTickSpacing(5);
-				slider.setMajorTickSpacing(10);
-					lewy.add(slider);
-		this.add(lewy, BorderLayout.LINE_START);
+			slider.setPaintTicks(true);
+			slider.setPaintLabels(true);
+			slider.setMinorTickSpacing(5);
+			slider.setMajorTickSpacing(10);
+			slider.setMaximumSize(new Dimension(150,200));
+			slider.setAlignmentX(Component.CENTER_ALIGNMENT);
+			
+			lewy.add(slider);
+			this.add(lewy, BorderLayout.LINE_START);
+			
 			prawy = new JPanel();
-				prawy.setBackground(Color.white); //na razie ustawiam bialy, ale trzeba pomyslec
-				//moze jakas grafike wstawimy tu?
-				//tez trzeba ogarnac jak kolor statku bd zmieniac (fajnie by bylo, jakby sie zmienial na tej grafice tez)
-				//albo wstawic grafike i na tej grafice wrzucic jakos zrobiony wielok¹t? XD
-		this.add(prawy, BorderLayout.CENTER);
-			gorny = new JPanel();
+			prawy.setPreferredSize(new Dimension(800,600));
+			prawy.setBackground(Color.white);
+			//prawy.setBorder(BorderFactory.createEmptyBorder(20,20,80,80));
+			//moze jakas grafike wstawimy tu?
+			this.add(prawy, BorderLayout.CENTER);
+			
+			//gorny = new JPanel();
 			menubar = new JMenuBar();
 			autor = new JMenu ("Autor");
 			wyswietl = new JMenuItem ("Wyœwietl informacje");
-				wyswietl.addActionListener(autorListener);
-					autor.add(wyswietl);
-					menubar.add(autor);
-		this.setJMenuBar(menubar);
-		this.add(gorny, BorderLayout.PAGE_START);
+			wyswietl.addActionListener(autorListener);
+			autor.add(wyswietl);
+			menubar.add(autor);
+			this.setJMenuBar(menubar);
+			//this.add(gorny, BorderLayout.PAGE_START);
+			this.pack();
+			this.setResizable(false);
 	}
 	ActionListener autorListener = new ActionListener() {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			JOptionPane.showMessageDialog(null, "Iga Michalska, Jan £oziñski");
-			
 		}
 	};
-
-	public GUI(GraphicsConfiguration arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
-
-	public GUI(String arg0) throws HeadlessException {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
-
-	public GUI(String arg0, GraphicsConfiguration arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
 
 	public static void main(String[] args) {
 		GUI frame = new GUI();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
-
 	}
-
 }
